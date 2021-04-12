6754feaa83ddcdaed8bdf2cf0226f6c2a09dcd49
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-12 16:38:00 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ Freenove.Initialization(DigitalPin.P0, DigitalPin.P1, DigitalPin.P2)
 basic.forever(function () {
     for (let index = 0; index <= 9; index++) {
         Freenove.Show_Number(9 - index)
+        basic.showNumber(9 - index)
         basic.pause(500)
     }
 })
